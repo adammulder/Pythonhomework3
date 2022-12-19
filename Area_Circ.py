aff70c8e32a33c2ca589f5978bfe8132a2cd2149
Adam Mulder <adammulder1@hotmail.com>
2022-12-19 15:56:26 -0500

    first commit

---
+++ Area_Circ.py
@@ -0,0 +1,12 @@
+from math import pi
+
+def areaCalc(length, width):
+    area= length * width
+    print(area)
+
+
+def circumferenceCalc(radius):
+    circ= 2* pi* radius
+    print(circ)
+
+
---
+++ module.py
@@ -0,0 +1,2 @@
+# def printName(name):
+#     return print(f"Hello Mr/Ms {name}...we've been waiting for you!")
